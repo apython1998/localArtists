066a6a085703dc30a6efc1e010adda20e02be9d1
apython1998 <alex.python1998@gmail.com>
2018-09-07 12:28:03 -0400

    Lab 2 Finalized

--- app/routes.py
+++ app/routes.py
@@ -1,41 +1,49 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
 
+
 @app.route('/artist/<artist_name>')
 def view_artist(artist_name):
     artist = {
-        "artist_id": 1,
-        "artist_name": artist_name,
-        "artist_info": "some fake artist info"
+        "name": artist_name,
+        "hometown": "City, State",
+        "info": "some fake artist info",
+        "events": ["Fake Event", "Fake Event 2"]
     }
     return render_template("artist.html", artist=artist)
+
 
 @app.route('/artists')
 def list_artists():
     artist_list = [
         {
-            "artist_id": 1,
-            "artist_name": "Eric Clapton",
-            "artist_info": "Some info"
+            "name": "Eric Clapton",
+            "hometown": "City, State",
+            "info": "Some info",
+            "events": []
         },
         {
-            "artist_id": 2,
-            "artist_name": "The Beatles",
-            "artist_info": "Some info about the Beatles. John Lennon, Paul McCartney, George Harrison, and Ringo Starr are the best"
+            "name": "The Beatles",
+            "hometown": "City, State",
+            "info": "Some info about the Beatles. John Lennon, Paul McCartney, George Harrison, and Ringo Starr are the best",
+            "events": []
         },
         {
-            "artist_id": 3,
-            "artist_name": "Jon Bellion",
-            "artist_info": ""
+            "name": "Jon Bellion",
+            "hometown": "Smithtown, NY",
+            "info": "Some info",
+            "events": []
         }
     ]
     return render_template("artistList.html", artist_list=artist_list)
 
+
 @app.route('/newArtist')
-def create_artist():
+def new_artist():
     return render_template("newArtist.html")
