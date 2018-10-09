c61c6e12de004e3afc80732a4cc2320a8d4714b6
apython1998 <alex.python1998@gmail.com>
2018-10-09 13:19:21 -0400

    Lab 6 Completed

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,63 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from flask_login import current_user
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField, DateTimeField, \
+    SelectField, SelectMultipleField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email, Length, ValidationError
+from app.models import User
 
 
 class NewArtistForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    hometown = StringField('Hometown', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired(), Length(max=64)])
+    hometown = StringField('Hometown', validators=[DataRequired(), Length(max=64)])
+    description = TextAreaField('Description', validators=[DataRequired(), Length(max=256)])
     submit = SubmitField('Submit')
+
+
+class NewVenueForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=64)])
+    address = StringField('Address', validators=[DataRequired(), Length(max=256)])
+    submit = SubmitField('Submit')
+
+
+class NewEventForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=128)])
+    time = DateTimeField('Start Time', format='%Y-%m-%d %H:%M:%S')
+    venue = SelectField('Venue', coerce=int)
+    artists = SelectMultipleField('Artists', coerce=int)
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')  # text that goes in the button
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')  # text that goes in the button
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Username Taken! Please use a different username...')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Email in use! Please use a different email...')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')  # text that goes in the button
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None and user.username is not current_user.username:
+            raise ValidationError('Username Taken! Please use a different username...')
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,17 @@
 from flask import render_template, url_for, redirect, flash, request
+from flask_login import login_required, login_user, logout_user, current_user
 from app import app, db
-from app.models import Artist, Event, Venue, ArtistToEvent
-from app.forms import NewArtistForm
+from app.models import Artist, Event, Venue, ArtistToEvent, User
+from app.forms import NewArtistForm, LoginForm, RegistrationForm, EditProfileForm, \
+    NewEventForm, NewVenueForm
 from datetime import datetime
 from sqlalchemy import func
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template("index.html")
 
@@ -70,10 +74,11 @@ def view_artist(artist_name):
 @app.route('/artists')
 def list_artists():
     artist_list = Artist.query.order_by(func.lower(Artist.name)).all()
-    return render_template("artistList.html", artist_list=artist_list)
+    return render_template("artist_list.html", artist_list=artist_list)
 
 
 @app.route('/newArtist', methods=['GET', 'POST'])
+@login_required
 def new_artist():
     form = NewArtistForm()
     if form.validate_on_submit():
@@ -86,4 +91,103 @@ def new_artist():
             db.session.commit()
             flash('New Artist Successfully Submitted!')
             return render_template("artist.html", artist=form_artist)
-    return render_template("newArtist.html", title='New Artist', form=form)
+    return render_template("new_artist_form.html", title='New Artist', form=form)
+
+
+@app.route('/newEvent', methods=['GET', 'POST'])
+@login_required
+def new_event():
+    form = NewEventForm()
+    form.venue.choices = [(v.id, v.name) for v in Venue.query.distinct().order_by(Venue.name).all()]
+    form.artists.choices = [(a.id, a.name) for a in Artist.query.distinct().order_by(Artist.name).all()]
+    if form.validate_on_submit():
+        form_event = Event(name=form.name.data, time=form.time.data, venue_id=form.venue.data)
+        db.session.add(form_event)
+        db.session.commit()
+        for artist_id in form.artists.data:
+            form_a_to_e = ArtistToEvent(artist_id=artist_id, event_id=form_event.id)
+            db.session.add(form_a_to_e)
+        db.session.commit()
+        flash('New Event Successfully Submitted!')
+        return redirect(url_for('index'))
+    return render_template('new_event_form.html', title='New Event', form=form)
+
+
+@app.route('/newVenue', methods=['GET', 'POST'])
+@login_required
+def new_venue():
+    form = NewVenueForm()
+    if form.validate_on_submit():
+        if Venue.query.filter_by(address=form.address.data).count() > 0:
+            flash('Venue with that address has already been created!')
+            return redirect(url_for('new_venue'))
+        form_venue = Venue(name=form.name.data, address=form.address.data)
+        db.session.add(form_venue)
+        db.session.commit()
+        flash('New Venue Successfully Submitted!')
+        return redirect(url_for('index'))
+    return render_template('new_venue_form.html', title='New Venue', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid Username or Password')
+            return redirect(url_for('login'))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        login_user(user=user, remember=form.remember_me.data)
+        return redirect(next_page)
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        newuser = User(username=form.username.data, email=form.email.data)
+        newuser.set_password(form.password.data)
+        db.session.add(newuser)
+        db.session.commit()
+        flash('Congratulations! You are now registered')
+        login_user(newuser)
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('user', username=current_user.username))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile', form=form)
+
